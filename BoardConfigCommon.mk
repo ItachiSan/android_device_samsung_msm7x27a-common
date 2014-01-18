164a75aa57bfbb64444920dd9cf77dbafce640d5
erikcas <erikcas1972@gmail.com>
2014-01-18 08:23:21 +0100

    test

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -143,7 +143,7 @@ BOARD_LPM_BOOT_ARGUMENT_VALUE := batt
 BOARD_CHARGER_RES := device/samsung/msm7x27a-common/res/charger
 
 ## Use device specific modules
-TARGET_PROVIDES_LIBLIGHTS := true
+TARGET_PROVIDES_LIBLIGHT := true
 TARGET_PROVIDES_LIBAUDIO := true
 
 ## Recovery
